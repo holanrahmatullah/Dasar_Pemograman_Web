63af1574b393365be2fc28d6595c91be0bd4ecc5
holanrahmatullah <holan1700016004@webmail.uad.ac.id>
2020-05-26 01:13:07 +0700

    css selesai

---
+++ assets/kalkulator.js
@@ -0,0 +1,1 @@
+    console.log("selamat anda bisa mmengunakan java scrip dengan mudah dan cepat aahh kapan lagi bisa belaja kaya gini kan,dicoding menambah ilmu porgraming saya holan rahmatullah suhut nadenggan hilan rahmatullah suhut nadenggan holan rahmatullah suhut nadenggan ")
